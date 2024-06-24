9975b85d772e11ee18549e0ae5cb69338a725186
Emanuelle <emanuellecortezc@gmail.com>
2024-06-24 13:19:05 -0300

    Rotas de clientes

--- api-rest-codeigniter/app/Config/Routes.php
+++ api-rest-codeigniter/app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', ['namespace' => 'App\Controllers\API'], function($routes){
+
+    $routes->get('clientes', 'CustomersController::index');
+    $routes->get('clientes/(:num)', 'CustomersController::show/$1');
+    $routes->post('clientes', 'CustomersController::create');
+    $routes->put('clientes/(:num)', 'CustomersController::update/$1');
+    $routes->delete('clientes/(:num)', 'CustomersController::delete/$1');
+
+});
